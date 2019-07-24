cc555fa0472f4f8827b1eb64ad0f4cd189dc7fb6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-24 16:35:33 +0000

    Deploy rate.ophie.io to github.com/edmundophie/currency-board-ui.git:gh-pages

---
+++ precache-manifest.fb767c695b6a8ef8d12235099ad69c03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b3da31cf72e72422883a23848cdaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8376eb9753cb2adc82",
+    "url": "/static/css/main.054b7696.chunk.css"
+  },
+  {
+    "revision": "799f69f1277015682af2",
+    "url": "/static/js/2.9f91a5f4.chunk.js"
+  },
+  {
+    "revision": "7a8376eb9753cb2adc82",
+    "url": "/static/js/main.07c9847b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git add .
-timestamp="$(date +%s)"
-git commit -m "auto commit #$timestamp"
-git push
